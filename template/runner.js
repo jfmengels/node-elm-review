f38beac9a96d30a23c56d7c8ac7363057f4527f2
Jeroen Engels <jfm.engels@gmail.com>
2017-06-18 23:24:18 +0200

    Add ESLint and clean up

--- template/runner.js
+++ template/runner.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 
-const getElmFiles = require('./elm-files')
+const getElmFiles = require('./elm-files');
 const Elm = require('./compiledLintApp');
 
 const elmFiles = getElmFiles([]);
 if (elmFiles.length === 0) {
-  console.error('Could not find any files to lint.');
+  console.error('Could not find any files to lint.'); // eslint-disable-line no-console
   process.exit(1);
 }
 
@@ -14,6 +14,6 @@ const app = Elm.LintApp.worker();
 app.ports.linting.send(elmFiles);
 
 app.ports.resultPort.subscribe(function(result) {
-  console.log(result.report);
+  console.log(result.report); // eslint-disable-line no-console
   process.exit(result.success ? 0 : 1);
 });
