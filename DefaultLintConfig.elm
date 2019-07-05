013cd919049bf870fd49b4b570d73067d789df4a
Jeroen Engels <jfm.engels@gmail.com>
2019-07-05 16:13:37 +0200

    Update the new default config

--- DefaultLintConfig.elm
+++ DefaultLintConfig.elm
@@ -1,38 +1,24 @@
 module LintConfig exposing (config)
 
-import Lint.Types exposing (LintRule, Severity(..))
-import Lint.Rules.DefaultPatternPosition
-import Lint.Rules.NoConstantCondition
-import Lint.Rules.NoDebug
-import Lint.Rules.NoDuplicateImports
-import Lint.Rules.NoExposingEverything
-import Lint.Rules.NoImportingEverything
-import Lint.Rules.NoNestedLet
-import Lint.Rules.NoUnannotatedFunction
-import Lint.Rules.NoUnusedVariables
-import Lint.Rules.NoUselessIf
-import Lint.Rules.NoUselessPatternMatching
-import Lint.Rules.NoWarningComments
-import Lint.Rules.SimplifyPiping
-import Lint.Rules.SimplifyPropertyAccess
-import Lint.Rules.ElmTest.NoDuplicateTestBodies
+-- Do not rename the module or the exposed function.
+-- `elm-lint` will look for these.
+
+import Lint exposing (Severity(..))
+import Lint.Rule exposing (Rule)
+import Lint.Rule.DefaultPatternPosition as DefaultPatternPosition
+import Lint.Rule.NoDebug
+import Lint.Rule.NoExtraBooleanComparison
+import Lint.Rule.NoImportingEverything
+import Lint.Rule.NoUnusedTypeConstructors
+import Lint.Rule.NoUnusedVariables
 
 
-config : List ( Severity, LintRule )
+config : List ( Severity, Rule )
 config =
-    [ ( Critical, Lint.Rules.DefaultPatternPosition.rule { position = Lint.Rules.DefaultPatternPosition.Last } )
-    , ( Critical, Lint.Rules.NoConstantCondition.rule )
-    , ( Critical, Lint.Rules.NoDebug.rule )
-    , ( Critical, Lint.Rules.NoDuplicateImports.rule )
-    , ( Critical, Lint.Rules.NoExposingEverything.rule )
-    , ( Critical, Lint.Rules.NoImportingEverything.rule { exceptions = [ "Html" ] } )
-    , ( Critical, Lint.Rules.NoNestedLet.rule )
-    , ( Critical, Lint.Rules.NoUnannotatedFunction.rule )
-    , ( Critical, Lint.Rules.NoUnusedVariables.rule )
-    , ( Critical, Lint.Rules.NoUselessIf.rule )
-    , ( Critical, Lint.Rules.NoUselessPatternMatching.rule )
-    , ( Warning, Lint.Rules.NoWarningComments.rule )
-    , ( Critical, Lint.Rules.SimplifyPiping.rule )
-    , ( Critical, Lint.Rules.SimplifyPropertyAccess.rule )
-    , ( Critical, Lint.Rules.ElmTest.NoDuplicateTestBodies.rule )
+    [ ( Disabled, DefaultPatternPosition.rule DefaultPatternPosition.ShouldBeLast )
+    , ( Critical, Lint.Rule.NoDebug.rule )
+    , ( Disabled, Lint.Rule.NoExtraBooleanComparison.rule )
+    , ( Disabled, Lint.Rule.NoImportingEverything.rule { exceptions = [] } )
+    , ( Critical, Lint.Rule.NoUnusedVariables.rule )
+    , ( Critical, Lint.Rule.NoUnusedTypeConstructors.rule )
     ]
