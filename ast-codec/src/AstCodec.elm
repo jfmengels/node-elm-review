8ab18c852e1b10f7ab2e0dce08b2bbab1f46d11d
Martin Stewart <MartinSStewart@gmail.com>
2020-08-03 18:44:16 +0200

    Finish codecs

--- ast-codec/src/AstCodec.elm
+++ ast-codec/src/AstCodec.elm
@@ -1,12 +1,30 @@
-module AstCodec exposing (expression, location, node, range)
+module AstCodec exposing (decode, encode)
 
-import Elm.Syntax.Expression exposing (Expression(..), Function, FunctionImplementation, LetBlock, LetDeclaration(..))
-import Elm.Syntax.Infix exposing (InfixDirection(..))
+import Elm.Syntax.Declaration exposing (Declaration(..))
+import Elm.Syntax.Exposing exposing (ExposedType, Exposing(..), TopLevelExpose(..))
+import Elm.Syntax.Expression exposing (CaseBlock, Expression(..), Function, FunctionImplementation, Lambda, LetBlock, LetDeclaration(..), RecordSetter)
+import Elm.Syntax.File exposing (File)
+import Elm.Syntax.Import exposing (Import)
+import Elm.Syntax.Infix exposing (Infix, InfixDirection(..))
+import Elm.Syntax.Module exposing (DefaultModuleData, EffectModuleData, Module(..))
 import Elm.Syntax.Node exposing (Node(..))
+import Elm.Syntax.Pattern exposing (Pattern(..), QualifiedNameRef)
 import Elm.Syntax.Range exposing (Location, Range)
 import Elm.Syntax.Signature exposing (Signature)
+import Elm.Syntax.Type exposing (Type, ValueConstructor)
+import Elm.Syntax.TypeAlias exposing (TypeAlias)
 import Elm.Syntax.TypeAnnotation exposing (RecordDefinition, TypeAnnotation(..))
 import Serialize as S exposing (Codec)
+
+
+encode : File -> String
+encode file_ =
+    S.encodeToString file file_
+
+
+decode : String -> Result (S.Error DecodeError) File
+decode data =
+    S.decodeFromString file data
 
 
 location : Codec e Location
@@ -163,11 +181,32 @@ expression =
         |> S.finishCustomType
 
 
+caseBlock : Codec DecodeError CaseBlock
+caseBlock =
+    S.record CaseBlock
+        |> S.field .expression (node lazyExpression)
+        |> S.field .cases (S.list (S.tuple (node pattern) (node lazyExpression)))
+        |> S.finishRecord
+
+
+lambda : Codec DecodeError Lambda
+lambda =
+    S.record Lambda
+        |> S.field .args (S.list (node pattern))
+        |> S.field .expression (node lazyExpression)
+        |> S.finishRecord
+
+
+recordSetter : Codec DecodeError RecordSetter
+recordSetter =
+    S.tuple (node S.string) (node lazyExpression)
+
+
 letBlock : Codec DecodeError LetBlock
 letBlock =
     S.record LetBlock
         |> S.field .declarations (S.list (node letDeclaration))
-        |> S.field .expression (node expression)
+        |> S.field .expression (node lazyExpression)
         |> S.finishRecord
 
 
@@ -183,7 +222,7 @@ letDeclaration =
                     e1 a b
         )
         |> S.variant1 LetFunction function
-        |> S.variant2 LetDestructuring (node pattern) (node expression)
+        |> S.variant2 LetDestructuring (node pattern) (node lazyExpression)
         |> S.finishCustomType
 
 
@@ -254,33 +293,266 @@ functionImplementation =
     S.record FunctionImplementation
         |> S.field .name (node S.string)
         |> S.field .arguments (S.list (node pattern))
-        |> S.field .expression (node expression)
+        |> S.field .expression (node lazyExpression)
         |> S.finishRecord
 
 
+pattern : Codec DecodeError Pattern
+pattern =
+    S.customType
+        (\e0 e1 e2 e3 e4 e5 e6 e7 e8 e9 e10 e11 e12 e13 e14 value ->
+            case value of
+                AllPattern ->
+                    e0
 
---{ name : Node String
---    , arguments : List (Node Pattern)
---    , expression : Node Expression
---    }
---type alias RecordDefinition =
---    List (Node RecordField)
---
---
---{-| Single field of a record. A name and its type.
----}
---type alias RecordField =
---    ( Node String, Node TypeAnnotation )
---type TypeAnnotation
---    = GenericType String
---    | Typed (Node ( ModuleName, String )) (List (Node TypeAnnotation))
---    | Unit
---    | Tupled (List (Node TypeAnnotation))
---    | Record RecordDefinition
---    | GenericRecord (Node String) (Node RecordDefinition)
---    | FunctionTypeAnnotation (Node TypeAnnotation) (Node TypeAnnotation)
+                UnitPattern ->
+                    e1
+
+                CharPattern a ->
+                    e2 a
+
+                StringPattern a ->
+                    e3 a
+
+                IntPattern a ->
+                    e4 a
+
+                HexPattern a ->
+                    e5 a
+
+                FloatPattern a ->
+                    e6 a
+
+                TuplePattern a ->
+                    e7 a
+
+                RecordPattern a ->
+                    e8 a
+
+                UnConsPattern a b ->
+                    e9 a b
+
+                ListPattern a ->
+                    e10 a
+
+                VarPattern a ->
+                    e11 a
+
+                NamedPattern a b ->
+                    e12 a b
+
+                AsPattern a b ->
+                    e13 a b
+
+                ParenthesizedPattern a ->
+                    e14 a
+        )
+        |> S.variant0 AllPattern
+        |> S.variant0 UnitPattern
+        |> S.variant1 CharPattern char
+        |> S.variant1 StringPattern S.string
+        |> S.variant1 IntPattern S.int
+        |> S.variant1 HexPattern S.int
+        |> S.variant1 FloatPattern S.float
+        |> S.variant1 TuplePattern (S.list (node lazyPattern))
+        |> S.variant1 RecordPattern (S.list (node S.string))
+        |> S.variant2 UnConsPattern (node lazyPattern) (node lazyPattern)
+        |> S.variant1 ListPattern (S.list (node lazyPattern))
+        |> S.variant1 VarPattern S.string
+        |> S.variant2 NamedPattern qualifiedNameRef (S.list (node lazyPattern))
+        |> S.variant2 AsPattern (node lazyPattern) (node S.string)
+        |> S.variant1 ParenthesizedPattern (node lazyPattern)
+        |> S.finishCustomType
+
+
+lazyPattern : Codec DecodeError Pattern
+lazyPattern =
+    S.lazy (\() -> pattern)
+
+
+qualifiedNameRef : Codec e QualifiedNameRef
+qualifiedNameRef =
+    S.record QualifiedNameRef
+        |> S.field .moduleName (S.list S.string)
+        |> S.field .name S.string
+        |> S.finishRecord
 
 
 lazyExpression : Codec DecodeError Expression
 lazyExpression =
     S.lazy (\() -> expression)
+
+
+file : Codec DecodeError File
+file =
+    S.record File
+        |> S.field .moduleDefinition (node module_)
+        |> S.field .imports (S.list (node import_))
+        |> S.field .declarations (S.list (node declaration))
+        |> S.field .comments (S.list (node S.string))
+        |> S.finishRecord
+
+
+import_ : Codec e Import
+import_ =
+    S.record Import
+        |> S.field .moduleName (node (S.list S.string))
+        |> S.field .moduleAlias (S.maybe (node (S.list S.string)))
+        |> S.field .exposingList (S.maybe (node exposing_))
+        |> S.finishRecord
+
+
+module_ : Codec e Module
+module_ =
+    S.customType
+        (\e0 e1 e2 value ->
+            case value of
+                NormalModule a ->
+                    e0 a
+
+                PortModule a ->
+                    e1 a
+
+                EffectModule a ->
+                    e2 a
+        )
+        |> S.variant1 NormalModule defaultModuleData
+        |> S.variant1 PortModule defaultModuleData
+        |> S.variant1 EffectModule effectModuleData
+        |> S.finishCustomType
+
+
+effectModuleData : Codec e EffectModuleData
+effectModuleData =
+    S.record EffectModuleData
+        |> S.field .moduleName (node (S.list S.string))
+        |> S.field .exposingList (node exposing_)
+        |> S.field .command (S.maybe (node S.string))
+        |> S.field .subscription (S.maybe (node S.string))
+        |> S.finishRecord
+
+
+defaultModuleData : Codec e DefaultModuleData
+defaultModuleData =
+    S.record DefaultModuleData
+        |> S.field .moduleName (node (S.list S.string))
+        |> S.field .exposingList (node exposing_)
+        |> S.finishRecord
+
+
+exposing_ : Codec e Exposing
+exposing_ =
+    S.customType
+        (\e0 e1 value ->
+            case value of
+                All a ->
+                    e0 a
+
+                Explicit a ->
+                    e1 a
+        )
+        |> S.variant1 All range
+        |> S.variant1 Explicit (S.list (node topLevelExpose))
+        |> S.finishCustomType
+
+
+topLevelExpose : Codec e TopLevelExpose
+topLevelExpose =
+    S.customType
+        (\e0 e1 e2 e3 value ->
+            case value of
+                InfixExpose a ->
+                    e0 a
+
+                FunctionExpose a ->
+                    e1 a
+
+                TypeOrAliasExpose a ->
+                    e2 a
+
+                TypeExpose a ->
+                    e3 a
+        )
+        |> S.variant1 InfixExpose S.string
+        |> S.variant1 FunctionExpose S.string
+        |> S.variant1 TypeOrAliasExpose S.string
+        |> S.variant1 TypeExpose exposedType
+        |> S.finishCustomType
+
+
+exposedType : Codec e ExposedType
+exposedType =
+    S.record ExposedType
+        |> S.field .name S.string
+        |> S.field .open (S.maybe range)
+        |> S.finishRecord
+
+
+declaration : Codec DecodeError Declaration
+declaration =
+    S.customType
+        (\e0 e1 e2 e3 e4 e5 value ->
+            case value of
+                FunctionDeclaration a ->
+                    e0 a
+
+                AliasDeclaration a ->
+                    e1 a
+
+                CustomTypeDeclaration a ->
+                    e2 a
+
+                PortDeclaration a ->
+                    e3 a
+
+                InfixDeclaration a ->
+                    e4 a
+
+                Destructuring a b ->
+                    e5 a b
+        )
+        |> S.variant1 FunctionDeclaration function
+        |> S.variant1 AliasDeclaration typeAlias
+        |> S.variant1 CustomTypeDeclaration type_
+        |> S.variant1 PortDeclaration signature
+        |> S.variant1 InfixDeclaration infix_
+        |> S.variant2 Destructuring (node pattern) (node expression)
+        |> S.finishCustomType
+
+
+infix_ : Codec e Infix
+infix_ =
+    S.record Infix
+        |> S.field .direction (node infixDirection)
+        |> S.field .precedence (node S.int)
+        |> S.field .operator (node S.string)
+        |> S.field .function (node S.string)
+        |> S.finishRecord
+
+
+typeAlias : Codec e TypeAlias
+typeAlias =
+    S.record TypeAlias
+        |> S.field .documentation (S.maybe (node S.string))
+        |> S.field .name (node S.string)
+        |> S.field .generics (S.list (node S.string))
+        |> S.field .typeAnnotation (node typeAnnotation)
+        |> S.finishRecord
+
+
+type_ : Codec e Type
+type_ =
+    S.record Type
+        |> S.field .documentation (S.maybe (node S.string))
+        |> S.field .name (node S.string)
+        |> S.field .generics (S.list (node S.string))
+        |> S.field .constructors (S.list (node valueConstructor))
+        |> S.finishRecord
+
+
+valueConstructor : Codec e ValueConstructor
+valueConstructor =
+    S.record ValueConstructor
+        |> S.field .name (node S.string)
+        |> S.field .arguments (S.list (node typeAnnotation))
+        |> S.finishRecord
