58409d267e017cfc8f5ebdd6a1384e38b3830ca8
Martin Stewart <MartinSStewart@gmail.com>
2020-08-03 12:46:54 +0200

    Start adding elm-syntax codecs

---
+++ ast-codec/src/AstCodec.elm
@@ -0,0 +1,286 @@
+module AstCodec exposing (expression, location, node, range)
+
+import Elm.Syntax.Expression exposing (Expression(..), Function, FunctionImplementation, LetBlock, LetDeclaration(..))
+import Elm.Syntax.Infix exposing (InfixDirection(..))
+import Elm.Syntax.Node exposing (Node(..))
+import Elm.Syntax.Range exposing (Location, Range)
+import Elm.Syntax.Signature exposing (Signature)
+import Elm.Syntax.TypeAnnotation exposing (RecordDefinition, TypeAnnotation(..))
+import Serialize as S exposing (Codec)
+
+
+location : Codec e Location
+location =
+    S.record Location
+        |> S.field .row S.int
+        |> S.field .column S.int
+        |> S.finishRecord
+
+
+range : Codec e Range
+range =
+    S.record Range
+        |> S.field .start location
+        |> S.field .end location
+        |> S.finishRecord
+
+
+node : Codec e a -> Codec e (Node a)
+node codec =
+    S.customType
+        (\encoder (Node range_ value) ->
+            encoder range_ value
+        )
+        |> S.variant2 Node range codec
+        |> S.finishCustomType
+
+
+char : Codec DecodeError Char
+char =
+    S.string
+        |> S.mapValid
+            (\string ->
+                case String.toList string of
+                    head :: _ ->
+                        Ok head
+
+                    [] ->
+                        Err InvalidChar
+            )
+            String.fromChar
+
+
+type DecodeError
+    = InvalidChar
+
+
+infixDirection : Codec e InfixDirection
+infixDirection =
+    S.enum Left [ Right, Non ]
+
+
+expression : Codec DecodeError Expression
+expression =
+    S.customType
+        (\e0 e1 e2 e3 e4 e5 e6 e7 e8 e9 e10 e11 e12 e13 e14 e15 e16 e17 e18 e19 e20 e21 e22 e23 value ->
+            case value of
+                Application a ->
+                    e0 a
+
+                UnitExpr ->
+                    e1
+
+                OperatorApplication a b c d ->
+                    e2 a b c d
+
+                FunctionOrValue moduleName string ->
+                    e3 moduleName string
+
+                IfBlock a b c ->
+                    e4 a b c
+
+                PrefixOperator a ->
+                    e5 a
+
+                Operator a ->
+                    e6 a
+
+                Integer a ->
+                    e7 a
+
+                Hex a ->
+                    e8 a
+
+                Floatable a ->
+                    e9 a
+
+                Negation a ->
+                    e10 a
+
+                Literal a ->
+                    e11 a
+
+                CharLiteral a ->
+                    e12 a
+
+                TupledExpression a ->
+                    e13 a
+
+                ParenthesizedExpression a ->
+                    e14 a
+
+                LetExpression a ->
+                    e15 a
+
+                CaseExpression a ->
+                    e16 a
+
+                LambdaExpression a ->
+                    e17 a
+
+                RecordExpr a ->
+                    e18 a
+
+                ListExpr a ->
+                    e19 a
+
+                RecordAccess a b ->
+                    e20 a b
+
+                RecordAccessFunction a ->
+                    e21 a
+
+                RecordUpdateExpression a b ->
+                    e22 a b
+
+                GLSLExpression a ->
+                    e23 a
+        )
+        |> S.variant1 Application (S.list (node lazyExpression))
+        |> S.variant0 UnitExpr
+        |> S.variant4 OperatorApplication S.string infixDirection (node lazyExpression) (node lazyExpression)
+        |> S.variant2 FunctionOrValue (S.list S.string) S.string
+        |> S.variant3 IfBlock (node lazyExpression) (node lazyExpression) (node lazyExpression)
+        |> S.variant1 PrefixOperator S.string
+        |> S.variant1 Operator S.string
+        |> S.variant1 Integer S.int
+        |> S.variant1 Hex S.int
+        |> S.variant1 Floatable S.float
+        |> S.variant1 Negation (node lazyExpression)
+        |> S.variant1 Literal S.string
+        |> S.variant1 CharLiteral char
+        |> S.variant1 TupledExpression (S.list (node lazyExpression))
+        |> S.variant1 ParenthesizedExpression (node lazyExpression)
+        |> S.variant1 LetExpression letBlock
+        |> S.variant1 CaseExpression caseBlock
+        |> S.variant1 LambdaExpression lambda
+        |> S.variant1 RecordExpr (S.list (node recordSetter))
+        |> S.variant1 ListExpr (S.list (node lazyExpression))
+        |> S.variant2 RecordAccess (node lazyExpression) (node S.string)
+        |> S.variant1 RecordAccessFunction S.string
+        |> S.variant2 RecordUpdateExpression (node S.string) (S.list (node recordSetter))
+        |> S.variant1 GLSLExpression S.string
+        |> S.finishCustomType
+
+
+letBlock : Codec DecodeError LetBlock
+letBlock =
+    S.record LetBlock
+        |> S.field .declarations (S.list (node letDeclaration))
+        |> S.field .expression (node expression)
+        |> S.finishRecord
+
+
+letDeclaration : Codec DecodeError LetDeclaration
+letDeclaration =
+    S.customType
+        (\e0 e1 value ->
+            case value of
+                LetFunction a ->
+                    e0 a
+
+                LetDestructuring a b ->
+                    e1 a b
+        )
+        |> S.variant1 LetFunction function
+        |> S.variant2 LetDestructuring (node pattern) (node expression)
+        |> S.finishCustomType
+
+
+function : Codec DecodeError Function
+function =
+    S.record Function
+        |> S.field .documentation (S.maybe (node S.string))
+        |> S.field .signature (S.maybe (node signature))
+        |> S.field .declaration (node functionImplementation)
+        |> S.finishRecord
+
+
+signature =
+    S.record Signature
+        |> S.field .name (node S.string)
+        |> S.field .typeAnnotation (node typeAnnotation)
+        |> S.finishRecord
+
+
+typeAnnotation : Codec e TypeAnnotation
+typeAnnotation =
+    S.customType
+        (\e0 e1 e2 e3 e4 e5 e6 value ->
+            case value of
+                GenericType a ->
+                    e0 a
+
+                Typed a b ->
+                    e1 a b
+
+                Unit ->
+                    e2
+
+                Tupled a ->
+                    e3 a
+
+                Record a ->
+                    e4 a
+
+                GenericRecord a b ->
+                    e5 a b
+
+                FunctionTypeAnnotation a b ->
+                    e6 a b
+        )
+        |> S.variant1 GenericType S.string
+        |> S.variant2 Typed (node (S.tuple (S.list S.string) S.string)) (S.list (node lazyTypeAnnotation))
+        |> S.variant0 Unit
+        |> S.variant1 Tupled (S.list (node lazyTypeAnnotation))
+        |> S.variant1 Record recordDefinition
+        |> S.variant2 GenericRecord (node S.string) (node recordDefinition)
+        |> S.variant2 FunctionTypeAnnotation (node lazyTypeAnnotation) (node lazyTypeAnnotation)
+        |> S.finishCustomType
+
+
+lazyTypeAnnotation : Codec e TypeAnnotation
+lazyTypeAnnotation =
+    S.lazy (\() -> typeAnnotation)
+
+
+recordDefinition : Codec e RecordDefinition
+recordDefinition =
+    S.list (node (S.tuple (node S.string) (node lazyTypeAnnotation)))
+
+
+functionImplementation : Codec DecodeError FunctionImplementation
+functionImplementation =
+    S.record FunctionImplementation
+        |> S.field .name (node S.string)
+        |> S.field .arguments (S.list (node pattern))
+        |> S.field .expression (node expression)
+        |> S.finishRecord
+
+
+
+--{ name : Node String
+--    , arguments : List (Node Pattern)
+--    , expression : Node Expression
+--    }
+--type alias RecordDefinition =
+--    List (Node RecordField)
+--
+--
+--{-| Single field of a record. A name and its type.
+---}
+--type alias RecordField =
+--    ( Node String, Node TypeAnnotation )
+--type TypeAnnotation
+--    = GenericType String
+--    | Typed (Node ( ModuleName, String )) (List (Node TypeAnnotation))
+--    | Unit
+--    | Tupled (List (Node TypeAnnotation))
+--    | Record RecordDefinition
+--    | GenericRecord (Node String) (Node RecordDefinition)
+--    | FunctionTypeAnnotation (Node TypeAnnotation) (Node TypeAnnotation)
+
+
+lazyExpression : Codec DecodeError Expression
+lazyExpression =
+    S.lazy (\() -> expression)
