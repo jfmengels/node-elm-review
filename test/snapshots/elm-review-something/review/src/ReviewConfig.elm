4bec40cd06f4149bf621a99ffcc7ea2c0e913f81
Jeroen Engels <jfm.engels@gmail.com>
2020-08-09 17:54:09 +0200

    new-package: Add no debug rules to default configuration

--- test/snapshots/elm-review-something/review/src/ReviewConfig.elm
+++ test/snapshots/elm-review-something/review/src/ReviewConfig.elm
@@ -12,6 +12,8 @@ when inside the directory containing this file.
 -}
 
 import Documentation.ReadmeLinksPointToCurrentVersion
+import NoDebug.Log
+import NoDebug.TodoOrToString
 import NoExposingEverything
 import NoForbiddenWords
 import NoImportingEverything
@@ -25,12 +27,15 @@ import NoUnused.Modules
 import NoUnused.Parameters
 import NoUnused.Patterns
 import NoUnused.Variables
-import Review.Rule exposing (Rule)
+import Review.Rule as Rule exposing (Rule)
 
 
 config : List Rule
 config =
     [ Documentation.ReadmeLinksPointToCurrentVersion.rule
+    , NoDebug.Log.rule
+    , NoDebug.TodoOrToString.rule
+        |> Rule.ignoreErrorsForDirectories [ "tests/" ]
     , NoExposingEverything.rule
     , NoForbiddenWords.rule [ "REPLACEME" ]
     , NoImportingEverything.rule []
