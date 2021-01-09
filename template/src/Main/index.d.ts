1176f18f4611b2636eb3a0609726f01bcfe37873
Jeroen Engels <jfm.engels@gmail.com>
2021-01-09 11:44:27 +0100

    Regenerate TS types

--- template/src/Main/index.d.ts
+++ template/src/Main/index.d.ts
@@ -1,10 +1,11 @@
-type FromElm = { tag: "SomethingBlue" } | { tag: "Alert"; message: string } | { tag: "SendPresenceHeartbeat" }
-type ToElm = { field: number }
+type FromElm = { tag : "SomethingBlue" } | { tag : "Alert"; message : string } | { tag : "SendPresenceHeartbeat" }
 
 type Flags = null
 
-export type JsonObject = { [Key in string]?: JsonValue };
-export interface JsonArray extends Array<JsonValue> { }
+type ToElm = { field : number }
+
+export type JsonObject = {[Key in string]?: JsonValue};
+export interface JsonArray extends Array<JsonValue> {}
 /**
 Matches any valid JSON value.
 Source: https://github.com/sindresorhus/type-fest/blob/master/source/basic.d.ts
@@ -27,4 +28,4 @@ declare const Elm: {
     init(options: { node?: HTMLElement | null; flags: Flags }): ElmApp;
   };
 };
-export { Elm, Flags };
+export { Elm, Flags, FromElm, ToElm };
