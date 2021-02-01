ca65df453875dcd988f048671325c3ecf4d0166d
Jeroen Engels <jfm.engels@gmail.com>
2021-02-01 23:52:13 +0100

    Remove pre-built Elm parser app

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-output="build/parseElm.js"
-(cd parseElm && elm make src/ParseMain.elm --output=../$output --optimize)
-npx uglifyjs $output --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters,keep_fargs=false,unsafe_comps,unsafe' | npx uglifyjs --mangle --output=$output
