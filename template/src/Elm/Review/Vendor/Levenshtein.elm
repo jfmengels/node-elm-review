5de32c27088813e1a45d7e6a3d5a1e335c67beac
Jeroen Engels <jfm.engels@gmail.com>
2021-03-01 16:10:25 +0100

    Update levenshtein vendored dependency

--- template/src/Elm/Review/Vendor/Levenshtein.elm
+++ template/src/Elm/Review/Vendor/Levenshtein.elm
@@ -6,6 +6,8 @@ module Elm.Review.Vendor.Levenshtein exposing (distance)
 
 -}
 
+-- Copied from dasch/levenshtein v1.0.3
+
 import Array exposing (Array)
 import Elm.Review.Vendor.Table as Table exposing (Table)
 
@@ -18,51 +20,54 @@ distance str1 str2 =
         0
 
     else
-        helper
+        distanceHelper
             (Array.fromList (String.toList str1))
             (Array.fromList (String.toList str2))
 
 
-helper arr1 arr2 =
+distanceHelper : Array Char -> Array Char -> Int
+distanceHelper arr1 arr2 =
     let
-        lev : Table -> ( Int, Int ) -> ( Table, Int )
-        lev table ( i, j ) =
+        calculateEditDistanceForChars : Table -> ( Int, Int ) -> ( Table, Int )
+        calculateEditDistanceForChars table ( i, j ) =
             case ( Array.get (i - 1) arr1, Array.get (j - 1) arr2 ) of
                 ( Just chr1, Just chr2 ) ->
                     let
-                        indicator =
-                            if chr1 /= chr2 then
-                                1
-
-                            else
-                                0
-
                         ( table1, dist1 ) =
-                            Table.fetch ( i - 1, j ) lev table
+                            Table.fetch ( i - 1, j ) calculateEditDistanceForChars table
 
                         ( table2, dist2 ) =
-                            Table.fetch ( i, j - 1 ) lev table1
+                            Table.fetch ( i, j - 1 ) calculateEditDistanceForChars table1
 
                         ( table3, dist3 ) =
-                            Table.fetch ( i - 1, j - 1 ) lev table2
+                            Table.fetch ( i - 1, j - 1 ) calculateEditDistanceForChars table2
                     in
                     ( table3
-                    , min3
-                        (dist1 + 1)
-                        (dist2 + 1)
-                        (dist3 + indicator)
+                      -- A more optimized version of
+                      -- min (min (dist1 + 1) (dist2 + 1)) (dist3 + indicator)
+                    , if dist3 < dist1 then
+                        if dist3 < dist2 then
+                            if chr1 /= chr2 then
+                                dist3 + 1
+
+                            else
+                                dist3
+
+                        else
+                            dist2 + 1
+
+                      else if dist1 > dist2 then
+                        dist2 + 1
+
+                      else
+                        dist1 + 1
                     )
 
                 _ ->
                     ( table, max i j )
 
-        firstKey =
+        indecesForLastChars =
             ( Array.length arr1, Array.length arr2 )
     in
-    lev (Table.empty firstKey) firstKey
+    calculateEditDistanceForChars (Table.empty indecesForLastChars) indecesForLastChars
         |> Tuple.second
-
-
-min3 : comparable -> comparable -> comparable -> comparable
-min3 a b c =
-    min (min a b) c
