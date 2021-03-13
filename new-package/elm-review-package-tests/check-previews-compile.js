fbeb898c91a5b41e9906c00ad2f758f96d06396a
Jeroen Engels <jfm.engels@gmail.com>
2021-03-13 22:28:58 +0100

    Have check-previews-compile check whether preview dependencies are in sync with elm.json's

--- new-package/elm-review-package-tests/check-previews-compile.js
+++ new-package/elm-review-package-tests/check-previews-compile.js
@@ -4,6 +4,7 @@ const path = require('path');
 const Ansi = require('./helpers/ansi');
 const {execSync} = require('child_process');
 const {findPreviewConfigurations} = require('./helpers/find-configurations');
+const packageDependencies = require('../elm.json').dependencies;
 
 const root = path.dirname(__dirname);
 
@@ -12,6 +13,13 @@ const root = path.dirname(__dirname);
 findPreviewConfigurations().forEach(checkThatExampleCompiles);
 
 function checkThatExampleCompiles(exampleConfiguration) {
+  const exampleConfigurationElmJson = require(`${exampleConfiguration}/elm.json`);
+
+  checkDepsAreCompatible(
+    path.basename(exampleConfiguration),
+    exampleConfigurationElmJson.dependencies.direct
+  );
+
   try {
     execSync(`npx elm-review --config ${exampleConfiguration} --report=json`, {
       encoding: 'utf8',
@@ -55,4 +63,51 @@ and make the necessary changes to make it compile.`
 
 function success(config) {
   console.log(`${Ansi.green('✔')} ${path.relative(root, config)}/ compiles`);
+}
+
+function checkDepsAreCompatible(exampleConfiguration, previewDependencies) {
+  Object.entries(packageDependencies).forEach(([depName, constraint]) => {
+    if (!(depName in previewDependencies)) {
+      console.error(
+        `Dependency ${depName} is missing in the ${exampleConfiguration}/ configuration`
+      );
+      process.exit(1);
+    }
+
+    checkConstraint(
+      exampleConfiguration,
+      depName,
+      constraint,
+      previewDependencies[depName]
+    );
+    delete previewDependencies[depName];
+  });
+
+  const remainingKeys = Object.keys(previewDependencies);
+  if (remainingKeys.length !== 0) {
+    console.error(
+      `There are extraneous dependencies in the ${exampleConfiguration}/ configuration: ${remainingKeys}`
+    );
+    process.exit(1);
+  }
+}
+
+function checkConstraint(exampleConfiguration, depName, constraint, version) {
+  const [minVersion] = constraint.split(' <= v < ').map(splitVersion);
+  const previewVersion = splitVersion(version);
+  const isValid =
+    previewVersion[0] === minVersion[0] &&
+    (previewVersion[1] > minVersion[1] ||
+      (previewVersion[1] === minVersion[1] &&
+        previewVersion[2] >= minVersion[2]));
+  if (!isValid) {
+    console.error(
+      `The constraint for ${depName} in ${exampleConfiguration}/ is not in the expected range. It was ${version} but it should be in ${constraint} to be in sync with the package's elm.json's dependencies.`
+    );
+    process.exit(1);
+  }
+}
+
+function splitVersion(version) {
+  return version.split('.').map((n) => Number.parseInt(n, 10));
 }
