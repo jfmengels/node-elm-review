065a08552c90c0adc066c81a41f776bdbf2d5bd3
Jeroen Engels <jfm.engels@gmail.com>
2021-10-19 19:50:54 +0200

    Reduce number of rules in test

--- test/project-with-suppressed-errors/review/src/ReviewConfig.elm
+++ test/project-with-suppressed-errors/review/src/ReviewConfig.elm
@@ -11,23 +11,11 @@ when inside the directory containing this file.
 
 -}
 
-import NoUnused.CustomTypeConstructors
-import NoUnused.Dependencies
-import NoUnused.Exports
-import NoUnused.Modules
-import NoUnused.Parameters
-import NoUnused.Patterns
 import NoUnused.Variables
 import Review.Rule exposing (Rule)
 
 
 config : List Rule
 config =
-    [ NoUnused.CustomTypeConstructors.rule []
-    , NoUnused.Dependencies.rule
-    , NoUnused.Exports.rule
-    , NoUnused.Modules.rule
-    , NoUnused.Parameters.rule
-    , NoUnused.Patterns.rule
-    , NoUnused.Variables.rule
+    [ NoUnused.Variables.rule
     ]
