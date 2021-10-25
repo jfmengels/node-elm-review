84f478d381d197bee1685b0bc40ee9f38e67492f
Jeroen Engels <jfm.engels@gmail.com>
2021-10-25 23:46:03 +0200

    Use Jest to test elm-review --help

---
+++ test/help.test.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const spawnAsync = require('cross-spawn');
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+test('--help', async () => {
+    const output = await spawn('--help');
+    expect(output.stdout).toMatchSnapshot();
+})
+
+test('init --help', async () => {
+    const output = await spawn('init --help');
+    expect(output.stdout).toMatchSnapshot();
+})
+
+test('suppress --help', async () => {
+    const output = await spawn('suppress --help');
+    expect(output.stdout).toMatchSnapshot();
+})
+
+test('new-package --help', async () => {
+    const output = await spawn('new-package --help');
+    expect(output.stdout).toMatchSnapshot();
+})
+
+test('new-rule --help', async () => {
+    const output = await spawn('new-rule --help');
+    expect(output.stdout).toMatchSnapshot();
+})
+
+const cli = path.resolve(__dirname, "../bin/elm-review");
+function spawn(args) {
+    return exec(`${cli} ${args}`);
+}
