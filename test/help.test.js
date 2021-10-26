f953ca23b22212b7d1b3626b149ef4519e82e52d
Jeroen Engels <jfm.engels@gmail.com>
2021-10-26 16:58:40 +0200

    Use test helper in help.test.js

--- test/help.test.js
+++ test/help.test.js
@@ -1,35 +1,26 @@
-const path = require('path');
-const spawnAsync = require('cross-spawn');
-const util = require('util');
-const exec = util.promisify(require('child_process').exec);
-const version = require('../package.json').version;
+const TestCli = require('./jest-helpers/cli');
 
 test('--help', async () => {
-    const output = await spawn('--help');
-    expect(output.stdout).toMatchSnapshot();
+    const output = await TestCli.run('--help');
+    expect(output).toMatchSnapshot();
 })
 
 test('init --help', async () => {
-    const output = await spawn('init --help');
-    expect(output.stdout).toMatchSnapshot();
+    const output = await TestCli.run('init --help');
+    expect(output).toMatchSnapshot();
 })
 
 test('suppress --help', async () => {
-    const output = await spawn('suppress --help');
-    expect(output.stdout).toMatchSnapshot();
+    const output = await TestCli.run('suppress --help');
+    expect(output).toMatchSnapshot();
 })
 
 test('new-package --help', async () => {
-    const output = await spawn('new-package --help');
-    expect(output.stdout).toMatchSnapshot();
+    const output = await TestCli.run('new-package --help');
+    expect(output).toMatchSnapshot();
 })
 
 test('new-rule --help', async () => {
-    const output = await spawn('new-rule --help');
-    expect(output.stdout).toMatchSnapshot();
+    const output = await TestCli.run('new-rule --help');
+    expect(output).toMatchSnapshot();
 })
-
-const cli = path.resolve(__dirname, "../bin/elm-review");
-function spawn(args) {
-    return exec(`${cli} --FOR-TESTS ${args}`);
-}
