4fdb0cf8a04cff489fcbb954b89401f1883b4121
Jeroen Engels <jfm.engels@gmail.com>
2021-10-26 17:24:38 +0200

    Move more

--- test/review.test.js
+++ test/review.test.js
@@ -1,10 +1,18 @@
 const path = require('path');
 const TestCli = require('./jest-helpers/cli');
 
-test.only('Regular run from inside the project', async () => {
+test('Regular run from inside the project', async () => {
     const output = await TestCli.runAndExpectError(
         '',
         { project: "project-with-errors/" }
     );
     expect(output).toMatchSnapshot();
-})
+});
+
+test('Running using other configuration (without errors)', async () => {
+    const output = await TestCli.run(
+        '--config ../config-that-triggers-no-errors',
+        { project: "project-with-errors/" }
+    );
+    expect(output).toMatchSnapshot();
+});
