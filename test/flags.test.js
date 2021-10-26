f28eba02ea32043d68fd4c8217d732fe536e6f2b
Jeroen Engels <jfm.engels@gmail.com>
2021-10-26 17:02:23 +0200

    Use helper in flags.test.js

--- test/flags.test.js
+++ test/flags.test.js
@@ -1,25 +1,11 @@
-const path = require('path');
-const spawnAsync = require('cross-spawn');
-const util = require('util');
-const exec = util.promisify(require('child_process').exec);
+const TestCli = require('./jest-helpers/cli');
 
 test('Running with an unknown flag', async () => {
-    return expect(spawn('--watc')
-    .catch(err => {
-        throw err.stdout;
-    })
-    ).rejects.toMatchSnapshot();
-})
+    const output = await TestCli.runAndExpectError('--watc');
+    expect(output).toMatchSnapshot();
+});
 
 test('Running with an unknown shorthand flag', async () => {
-    return expect(spawn('-u')
-    .catch(err => {
-        throw err.stdout;
-    })
-    ).rejects.toMatchSnapshot();
-})
-
-const cli = path.resolve(__dirname, "../bin/elm-review");
-function spawn(args) {
-    return exec(`${cli} ${args}`);
-}
+    const output = await TestCli.runAndExpectError('-u');
+    expect(output).toMatchSnapshot();
+});
--- test/jest-helpers/cli.js
+++ test/jest-helpers/cli.js
@@ -6,11 +6,22 @@ const cli = path.resolve(__dirname, "../../bin/elm-review");
 
 module.exports = {
     run,
+    runAndExpectError,
     runWithoutTestMode
 }
 
 function run(args) {
     return internalExec(`--FOR-TESTS ${args}`);
+}
+
+async function runAndExpectError(args) {
+    try {
+        const output = await internalExec(`--FOR-TESTS ${args}`);
+        throw new Error(`CLI did not exit with an exit code as expected. Here is its output:\n\n${output}`);
+    }
+    catch(output) {
+        return output;
+    }
 }
 
 function runWithoutTestMode(args) {
