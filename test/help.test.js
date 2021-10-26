37b5bb35d315cc4e17a9ab2e72b5f5bddef5ceec
Jeroen Engels <jfm.engels@gmail.com>
2021-10-26 23:06:44 +0200

    Move snapshots

--- test/help.test.js
+++ test/help.test.js
@@ -1,26 +1,30 @@
 const TestCli = require('./jest-helpers/cli');
 
+function testName(name) {
+    return `test/snapshots/help/${name}.txt`;
+}
+
 test('--help', async () => {
     const output = await TestCli.run('--help');
-    expect(output).toMatchFile();
+    expect(output).toMatchFile(testName("default"));
 })
 
 test('init --help', async () => {
     const output = await TestCli.run('init --help');
-    expect(output).toMatchFile();
+    expect(output).toMatchFile(testName('init'));
 })
 
 test('suppress --help', async () => {
     const output = await TestCli.run('suppress --help');
-    expect(output).toMatchFile();
+    expect(output).toMatchFile(testName('suppress'));
 })
 
 test('new-package --help', async () => {
     const output = await TestCli.run('new-package --help');
-    expect(output).toMatchFile();
+    expect(output).toMatchFile(testName('new-package'));
 })
 
 test('new-rule --help', async () => {
     const output = await TestCli.run('new-rule --help');
-    expect(output).toMatchFile();
+    expect(output).toMatchFile(testName('new-rule'));
 })
