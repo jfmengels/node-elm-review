691fa8d003a04d99525906eccedf516050dc8e64
Jeroen Engels <jfm.engels@gmail.com>
2023-03-21 19:07:18 +0100

    Add --rule-type in tests and create a project rule in the tests

---
+++ test/run-snapshots/elm-review-something-for-new-rule/src/SomeModuleRule.elm
@@ -0,0 +1,71 @@
+module SomeModuleRule exposing (rule)
+
+{-|
+
+@docs rule
+
+-}
+
+import Elm.Syntax.Expression exposing (Expression)
+import Elm.Syntax.Node as Node exposing (Node)
+import Review.Rule as Rule exposing (Rule)
+
+
+{-| Reports... REPLACEME
+
+    config =
+        [ SomeModuleRule.rule
+        ]
+
+
+## Fail
+
+    a =
+        "REPLACEME example to replace"
+
+
+## Success
+
+    a =
+        "REPLACEME example to replace"
+
+
+## When (not) to enable this rule
+
+This rule is useful when REPLACEME.
+This rule is not useful when REPLACEME.
+
+
+## Try it out
+
+You can try this rule out by running the following command:
+
+```bash
+elm-review --template some-author/elm-review-something/example --rules SomeModuleRule
+```
+
+-}
+rule : Rule
+rule =
+    Rule.newModuleRuleSchemaUsingContextCreator "SomeModuleRule" initialContext
+        |> Rule.withExpressionEnterVisitor expressionVisitor
+        |> Rule.fromModuleRuleSchema
+
+
+type alias Context =
+    {}
+
+
+initialContext : Rule.ContextCreator () Context
+initialContext =
+    Rule.initContextCreator
+        (\() ->
+            {}
+        )
+
+
+expressionVisitor : Node Expression -> Context -> ( List (Rule.Error {}), Context )
+expressionVisitor node context =
+    case Node.value node of
+        _ ->
+            ( [], context )
