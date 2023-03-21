d8fe9984c61d003c13e5e82d6758baa7a5f06649
Jeroen Engels <jfm.engels@gmail.com>
2023-03-21 18:31:19 +0100

    Add a test by default that does not expect errors

--- test/run-snapshots/elm-review-something-for-new-rule/tests/SomeRuleTest.elm
+++ test/run-snapshots/elm-review-something-for-new-rule/tests/SomeRuleTest.elm
@@ -8,7 +8,14 @@ import Test exposing (Test, describe, test)
 all : Test
 all =
     describe "SomeRule"
-        [ test "should report an error when REPLACEME" <|
+        [ test "should not report an error when REPLACEME" <|
+            \() ->
+                """module A exposing (..)
+a = 1
+"""
+                    |> Review.Test.run rule
+                    |> Review.Test.expectNoErrors
+        , test "should report an error when REPLACEME" <|
             \() ->
                 """module A exposing (..)
 a = 1
