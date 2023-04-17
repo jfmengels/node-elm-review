2c5a1c4dc2ed02981510f0906827f77884661404
Jeroen Engels <jfm.engels@gmail.com>
2023-04-17 23:29:58 +0200

    Improve the 'Unsupported elm-review version' message (#116)

---
+++ test/path-helpers.test.js
@@ -0,0 +1,25 @@
+const PathHelpers = require('../lib/path-helpers');
+
+test('should leave the path untouched if it does not contain odd characters', () => {
+  const input = 'some-folder123';
+  const output = PathHelpers.format(input);
+  expect(output).toEqual(input);
+});
+
+test('should escape single quotes', () => {
+  const input = "Don't-do-that";
+  const output = PathHelpers.format(input);
+  expect(output).toEqual('Don\\\'t-do-that');
+});
+
+test('should escape double quotes', () => {
+  const input = 'Don\"t-do-that';
+  const output = PathHelpers.format(input);
+  expect(output).toEqual('Don\\\"t-do-that');
+});
+
+test('should escape spaces', () => {
+  const input = "some path";
+  const output = PathHelpers.format(input);
+  expect(output).toEqual('some\\ path');
+});
