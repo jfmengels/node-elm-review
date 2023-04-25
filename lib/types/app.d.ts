7486612c61971b725e060e91b501fc0e2d878f29
Jeroen Engels <jfm.engels@gmail.com>
2023-04-25 17:32:16 +0200

    Add types for autofix

--- lib/types/app.d.ts
+++ lib/types/app.d.ts
@@ -1,6 +1,8 @@
 
 /**
  * @typedef { import("./types/options.d.ts").Options } Options
+ * @typedef { import("./types/path").Path } Path
+ * @typedef { import("./types/state").FilesProposedByCurrentFix } FilesProposedByCurrentFix
  * @typedef { import("./types/content").ElmFile } ElmFile
  * @typedef { import("./types/content").Ast } Ast
  * @typedef { import("./types/content").Readme } Readme
@@ -24,14 +26,27 @@ export type Ports = {
   collectFile: SendPort<ElmFile>,
   collectSuppressedErrors: SendPort<SuppressedErrorsFile[]>,
   collectLinks: SendPort<LinksToRuleDocs>,
+  userConfirmedFix: SendPort<FixConfirmation>,
 
-  acknowledgeFileReceipt: SubscribePort<FileReceipt>
+  acknowledgeFileReceipt: SubscribePort<FileReceipt>,
+  askConfirmationToFix: SubscribePort<AutofixRequest>,
 }
 
 export type FileReceipt = {
   path: Path,
   cacheRequest: { source: string, ast: Ast } | null
 }
+
+export type AutofixRequest = {
+  confirmationMessage: any,
+  clearFixLine: boolean,
+  changedFiles: any,
+  count: number
+}
+
+export type FixConfirmation
+  = { answer: false }
+  | { answer: true, files: FilesProposedByCurrentFix, dependencies: Dependencies }
 
 export type SubscribePort = function;
 export type SendPort<T> = { send: (data: T) => void };
