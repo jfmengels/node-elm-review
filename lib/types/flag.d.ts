1e17cf4fbcda73f11d352d7303c98bcc80d958a6
Jeroen Engels <jfm.engels@gmail.com>
2023-04-28 17:29:15 +0200

    Add type for Flag

---
+++ lib/types/flag.d.ts
@@ -0,0 +1,25 @@
+import type { Chalk } from 'chalk';
+
+export type Section = 'regular' | 'suppress' | 'fix' | 'init' | 'new-rule' | 'new-package' | 'hidden' | 'suppress-subcommand';
+
+export type Flag = {
+  name: string,
+  alias?: string,
+  color?: Chalk,
+  sections: Section[],
+  description: string[],
+  initDescription?: string[],
+  newPackageDescription?: string[],
+} & BooleanFlag;
+
+export type BooleanFlag
+  = {
+      boolean: true,
+      mayBeUsedSeveralTimes?: false
+    }
+  | {
+      boolean: false,
+      argName: string,
+      mayBeUsedSeveralTimes: boolean,
+      usesEquals: boolean,
+    }
