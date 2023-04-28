39b3f1d6c89618ae0b610e107f3d35bd44555ff0
Jeroen Engels <jfm.engels@gmail.com>
2023-04-28 16:47:26 +0200

    Add types for getRemoteElmJson

--- lib/types/build.d.ts
+++ lib/types/build.d.ts
@@ -3,12 +3,13 @@ import type { Path } from "./types/path"
 export type BuildResult = {
   elmModulePath: Path | null;
   reviewElmJsonPath: Path;
-  reviewElmJson: any;
+  reviewElmJson: ReviewElmJson;
   appHash: AppHash;
 }
 
 export type AppHash = string;
 
 export type ReviewElmJson = {
+  "source-directories": Array<Path>,
   dependencies: Record<string, string>
 };
