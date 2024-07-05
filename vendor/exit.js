ab0a65d42484c9d6afd6959249466f8f5c3defd0
Eli <88557639+lishaduck@users.noreply.github.com>
2024-07-05 09:33:25 -0400

    chore: enable formatting for `exit.js`

--- vendor/exit.js
+++ vendor/exit.js
@@ -14,7 +14,9 @@
  * @returns {never}
  */
 module.exports = function exit(exitCode, streams = []) {
-  if (!streams) { streams = [process.stdout, process.stderr]; }
+  if (!streams) {
+    streams = [process.stdout, process.stderr];
+  }
   var drainCount = 0;
   // Actually exit if all streams are drained.
   // @ts-expect-error - This is really `never | void`, but TS isn't smart enough to deal with that later on.
@@ -24,12 +26,12 @@ module.exports = function exit(exitCode, streams = []) {
       process.exit(exitCode);
     }
   }
-  streams.forEach(function(stream) {
+  streams.forEach(function (stream) {
     // Count drained streams now, but monitor non-drained streams.
     if (stream.writableLength === 0) {
       drainCount++;
     } else {
-      stream.write('', 'utf-8', function() {
+      stream.write('', 'utf-8', function () {
         drainCount++;
         tryToExit();
       });
@@ -42,7 +44,7 @@ module.exports = function exit(exitCode, streams = []) {
   // In Windows, when run as a Node.js child process, a script utilizing
   // this library might just exit with a 0 exit code, regardless. This code,
   // despite the fact that it looks a bit crazy, appears to fix that.
-  process.on('exit', function() {
+  process.on('exit', function () {
     process.exit(exitCode);
   });
 };
