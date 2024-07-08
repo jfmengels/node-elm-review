7d23dc07a038c089eb55cb38e81c61d244dd9a11
Eli <88557639+lishaduck@users.noreply.github.com>
2024-07-07 23:21:19 -0500

    chore: go overboard
    Make the type system as smart as the function.

---
+++ test/snapshotter.js
@@ -0,0 +1,12 @@
+/**
+ * @template {string} G
+ * @template {string} N
+ * @param {G} group
+ * @param {N} name
+ * @returns {`test/snapshots/${G}/${N}.txt`}
+ */
+function snapshotPath(group, name) {
+  return `test/snapshots/${group}/${name}.txt`;
+}
+
+module.exports = {snapshotPath};
