225a4914c95aa472914e97eaf42b3c4609310c51
Eli <88557639+lishaduck@users.noreply.github.com>
2024-09-23 11:57:46 -0500

    ci: debug

--- test/suppress.test.js
+++ test/suppress.test.js
@@ -54,6 +54,7 @@ test('Running with "suppress --check-after-tests" when there are uncommitted cha
   );
   // Remove uncommitted suppression files
   childProcess.execSync(`git checkout HEAD ${folder}`);
+  throw output;
   expect(output).toMatchFile(
     testName('suppressed-errors-check-with-uncommitted-changes')
   );
