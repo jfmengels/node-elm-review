834c54aeb6ab30c66b827e5c1f09d191e2d2d038
Eli <88557639+lishaduck@users.noreply.github.com>
2024-11-03 22:47:14 -0600

    chore: enable `noImplicitAny` on `result-cache.js`

---
+++ lib/types/result-cache.ts
@@ -0,0 +1,32 @@
+import type {Replacer} from './json.js';
+import type {Path} from './path.ts';
+
+declare global {
+  // eslint-disable-next-line no-var -- See discussion at typescript-eslint/typescript-eslint#7941.
+  var loadResultFromCache:
+    | (() => null)
+    | ((ruleName: RuleName, ruleId: RuleId) => CacheEntry);
+
+  // eslint-disable-next-line no-var -- See discussion at typescript-eslint/typescript-eslint#7941.
+  var saveResultToCache:
+    | (() => void)
+    | ((
+        ruleName: RuleName,
+        ruleId: RuleId,
+        cacheEntry: CacheEntry
+      ) => Promise<void>);
+
+  // eslint-disable-next-line no-var -- See discussion at typescript-eslint/typescript-eslint#7941.
+  var elmJsonReplacer: Replacer;
+}
+
+export type RuleName = string;
+export type RuleId = string;
+export type CacheEntry = unknown;
+export type CacheKey = `${RuleName}-${RuleId}}`;
+
+export type CacheData = {
+  filePath: Path;
+  cacheEntry: CacheEntry;
+  cacheKey: CacheKey;
+};
