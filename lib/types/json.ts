9f2916915c334cef0a5f382dfa2ca67e3a1e9c8d
Eli <88557639+lishaduck@users.noreply.github.com>
2024-12-01 22:05:18 -0600

    wip: begin improving testing story

--- lib/types/json.ts
+++ lib/types/json.ts
@@ -1,9 +1,9 @@
 /**
- * A replacer function for JSON.stringify.
+ * A replacer function for {@linkcode JSON.stringify}.
  */
 export type Replacer = (key: string, value: unknown) => unknown;
 
 /**
- * A reviver function for JSON.parse.
+ * A reviver function for {@linkcode JSON.parse}.
  */
 export type Reviver = (key: string, value: unknown) => unknown;
