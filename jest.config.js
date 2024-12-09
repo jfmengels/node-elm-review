653a00a4e54f506cf78777c6bcaf191b6beb4339
Eli <88557639+lishaduck@users.noreply.github.com>
2024-12-08 20:41:48 -0600

    fix: ignore temp dir from haste
    Haste is an ancient, deprecated, and unmaintained module system,
    which happens to power Jest.
    It doesn't like that there are duplicate manifests in the temp and snapshot dirs.

--- jest.config.js
+++ jest.config.js
@@ -84,7 +84,10 @@ module.exports = {
   // moduleNameMapper: {},
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
-  // modulePathIgnorePatterns: [],
+  modulePathIgnorePatterns: [
+    '<rootDir>/test/temporary/',
+    '<rootDir>/test/run-snapshots/'
+  ],
 
   // Activates notifications for test results
   // notify: false,
