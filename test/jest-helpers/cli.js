962911a2f631d55ad109abcbe0ee42c9a6744a2a
Eli <88557639+lishaduck@users.noreply.github.com>
2024-12-08 21:43:54 -0600

    refactor: use zx for jest tests

--- test/jest-helpers/cli.js
+++ test/jest-helpers/cli.js
@@ -1,72 +1,77 @@
+/**
+ * @import {ProcessOutput} from 'zx' with {'resolution-mode': 'import'};
+ * @import {Options} from './types/cli';
+ */
+
 const path = require('node:path');
-const {promisify} = require('node:util');
 const {toMatchFile} = require('jest-file-snapshot');
-const exec = promisify(require('node:child_process').exec);
+// @ts-expect-error(TS1479): zx doesn't ship CJS types.
+const {$} = require('zx');
 
 const cli = path.resolve(__dirname, '../../bin/elm-review');
 expect.extend({toMatchFile});
 
 /**
- * @import {Options} from './types/cli';
- */
-
-/**
- * @param {string} args
+ * @param {string[]} args
  * @param {Options} [options]
  * @returns {Promise<string>}
  */
 async function run(args, options) {
-  return await internalExec(`--FOR-TESTS ${args}`, options);
+  const output = await internalExec(['--FOR-TESTS', ...args], options);
+
+  if (output.exitCode !== 0) throw new Error(output.text());
+
+  return output.stdout;
 }
 
 /**
- * @param {string} args
+ * @param {string[]} args
  * @param {Options | undefined} [options]
  * @returns {Promise<unknown>}
  */
 async function runAndExpectError(args, options) {
-  try {
-    const output = await internalExec(`--FOR-TESTS ${args}`, options);
-    throw new Error(
-      `CLI did not exit with an exit code as expected. Here is its output:\n\n${output}`
-    );
-  } catch (/** @type {unknown} */ error) {
-    return error;
+  const output = await internalExec(['--FOR-TESTS', ...args], options);
+  if (output.exitCode !== 0) {
+    return output.stdout; // Should this be stderr?
   }
+
+  throw new Error(
+    `CLI did not exit with an exit code as expected. Here is its output:
+
+  ${output.text()}`
+  );
 }
 
 /**
- * @param {string} args
+ * @param {string[]} args
  * @param {Options | undefined} [options]
  * @returns {Promise<string>}
  */
 async function runWithoutTestMode(args, options) {
-  return await internalExec(args, options);
+  const output = await internalExec(args, options);
+
+  if (output.exitCode !== 0) throw new Error(output.text());
+
+  return output.stdout;
 }
 
 /**
- * @param {string} args
+ * @param {string[]} args
  * @param {Options} [options]
- * @returns {Promise<string>}
+ * @returns {Promise<ProcessOutput>}
  */
 async function internalExec(args, options = {}) {
-  // Overriding FORCE_COLOR because Jest forcefully adds it as well,
-  // which otherwise enables colors when we don't want it.
-  const colorFlag = 'FORCE_COLOR=' + (options.colors ? '1' : '0');
-
-  try {
-    // If this just uses child_process.exec, the shell scripts are pointless, and should be all migrated to Jest tests.
-    const result = await exec(
-      [colorFlag, cli, reportMode(options), colors(options), args].join(' '),
-      {
-        ...options,
-        cwd: cwdFromOptions(options)
-      }
-    );
-    return result.stdout;
-  } catch (error) {
-    throw error.stdout;
-  }
+  const result = await $({
+    cwd: cwdFromOptions(options),
+    env: {
+      ...process.env,
+      // Overriding `FORCE_COLOR` because Jest forcefully adds it as well,
+      // which otherwise enables colors when we don't want it.
+      FORCE_COLOR: options.colors ? '1' : '0'
+    },
+    quiet: true
+  })`${cli} ${reportMode(options)} ${colors(options)} ${args}`.nothrow();
+  return result;
 }
 
 /**
@@ -83,26 +88,26 @@ function cwdFromOptions(options) {
 
 /**
  * @param {Options} options
- * @returns {string}
+ * @returns {string[]}
  */
 function reportMode(options) {
   if (!options.report) {
-    return '';
+    return [];
   }
 
-  return `--report=${options.report}`;
+  return [`--report=${options.report}`];
 }
 
 /**
  * @param {Options} options
- * @returns {"" | "--no-color"}
+ * @returns {string[]}
  */
 function colors(options) {
   if (options.colors) {
-    return '';
+    return [];
   }
 
-  return `--no-color`;
+  return ['--no-color'];
 }
 
 module.exports = {
