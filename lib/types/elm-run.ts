e21a5ac7c2b1958dbc0b9cd267e89e33bf0dfa2d
Jeroen Engels <jfm.engels@gmail.com>
2026-03-20 15:33:44 +0100

    Add compileElmProjectUsingElmRun

---
+++ lib/types/elm-run.ts
@@ -0,0 +1,15 @@
+import type {Path} from '../../lib/types/path';
+
+export type ElmRunOptions = {
+  cwd: Path;
+  output: Path;
+  target: Path;
+  verbose: boolean;
+  pathToElmRun: Path;
+  processOpts: ElmRunProcessOptions;
+};
+
+export interface ElmRunProcessOptions {
+  env: NodeJS.ProcessEnv;
+  stdio: ('ignore' | 'inherit' | 'pipe')[];
+}
