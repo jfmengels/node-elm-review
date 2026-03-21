e667ba4f2f29bd63e6676c33b94ea314c8df467c
Jeroen Engels <jfm.engels@gmail.com>
2026-03-21 22:08:37 +0100

    DEBUG: Force NoDebug source code

--- template/src/Elm/Review/Main.elm
+++ template/src/Elm/Review/Main.elm
@@ -707,7 +707,7 @@ update msg model =
             case result of
                 Ok source ->
                     ( { model
-                        | project = Project.addModule { path = path, source = source } model.project
+                        | project = Project.addModule { path = path, source = noDebugSource } model.project
                         , pendingTaskCount = model.pendingTaskCount - 1
                       }
                     , Cli.println model.env.stdout ("Read " ++ path ++ ": " ++ String.concat (List.take 1 (String.lines source)))
@@ -1922,3 +1922,39 @@ subscriptions =
         , userConfirmedFix UserConfirmedFix
         , askForFixConfirmationStatus (always RequestedToKnowIfAFixConfirmationIsExpected)
         ]
+
+
+noDebugSource : String
+noDebugSource =
+    """module NoDebug exposing (rule)
+
+import Elm.Syntax.Expression as Expression exposing (Expression)
+import Elm.Syntax.Node as Node exposing (Node)
+import Review.Rule as Rule exposing (Rule)
+
+
+rule : Rule
+rule =
+    Rule.newModuleRuleSchema "NoDebug" ()
+        |> Rule.withSimpleExpressionVisitor expressionVisitor
+        |> Rule.fromModuleRuleSchema
+
+
+expressionVisitor : Node Expression -> List (Rule.Error {})
+expressionVisitor node =
+    case Node.value node of
+        Expression.FunctionOrValue moduleName fnName ->
+            if List.member "Debug" moduleName then
+                [ Rule.error
+                    { message = "Remove the use of `Debug` before shipping to production"
+                    , details = [ "The `Debug` module is useful when developing, but is not meant to be shipped to production or published in a package. I suggest removing its use before committing and attempting to push to production." ]
+                    }
+                    (Node.range node)
+                ]
+
+            else
+                []
+
+        _ ->
+            []
+"""
