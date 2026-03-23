82a71ab8fdb81461c4450e51d8345d10a754c747
Jeroen Engels <jfm.engels@gmail.com>
2026-03-23 17:50:17 +0100

    Add RunEnvironment

---
+++ template/src/Elm/Review/RunEnvironment.elm
@@ -0,0 +1,13 @@
+module Elm.Review.RunEnvironment exposing (RunEnvironment)
+
+{-| Information about the environment in which the review application runs.
+
+@docs RunEnvironment
+
+-}
+
+
+type alias RunEnvironment =
+    { elmVersion : String
+    , elmHomePath : String
+    }
