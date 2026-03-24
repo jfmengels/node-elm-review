eb265e60bce76af2bb5ec577160c8408b43633a9
Jeroen Engels <jfm.engels@gmail.com>
2026-03-24 17:32:08 +0100

    Move Flags to another module

--- template/src/Elm/Review/Main.elm
+++ template/src/Elm/Review/Main.elm
@@ -12,6 +12,7 @@ import Elm.Review.Color as Color
 import Elm.Review.File
 import Elm.Review.FileFetch as FileFetch
 import Elm.Review.FixExplanation as FixExplanation exposing (FixExplanation)
+import Elm.Review.Flags as Flags exposing (FixMode(..), Flags, ReportMode(..))
 import Elm.Review.RefusedErrorFixes as RefusedErrorFixes exposing (RefusedErrorFixes)
 import Elm.Review.Reporter as Reporter
 import Elm.Review.RunEnvironment as RunEnvironment exposing (RunEnvironment)
@@ -133,10 +134,6 @@ abortWithDetails env supportsColor { title, message } =
 -- PROGRAM
 
 
-type alias Flags =
-    Encode.Value
-
-
 main : Cli.Program ModelWrapper Msg
 main =
     Cli.program
@@ -202,12 +199,6 @@ type AwaitingConfirmation
     | AwaitingFixAll
 
 
-type FixMode
-    = Mode_DontFix
-    | Mode_Fix Bool
-    | Mode_FixAll Bool
-
-
 toReviewOptionsFixMode : Bool -> Model -> ReviewOptions.FixMode
 toReviewOptionsFixMode fixAllAllowed model =
     if not fixAllAllowed then
@@ -248,11 +239,6 @@ isFileRemovalFixesEnabled fixMode =
             fileRemovalFixesEnabled
 
 
-type ReportMode
-    = HumanReadable
-    | Json
-
-
 type ModelWrapper
     = Done
     | Running Model
@@ -270,7 +256,7 @@ init env =
 
         Ok fs ->
             let
-                flags : DecodedFlags
+                flags : Flags
                 flags =
                     -- TODO Decode flags from parent process
                     -- case Decode.decodeValue decodeFlags rawFlags of
@@ -281,31 +267,14 @@ init env =
                             )
 
                         Err error ->
-                            ({ fixMode = Mode_DontFix
-                             , fixLimit = Nothing
-                             , enableExtract = False
-                             , fixExplanation = FixExplanation.Succinct
-                             , unsuppressMode = UnsuppressMode.UnsuppressNone
-                             , reportMode = HumanReadable
-                             , detailsMode = Reporter.WithDetails
-                             , ignoreProblematicDependencies = False
-                             , rulesFilter = Nothing
-                             , ignoredDirs = []
-                             , ignoredFiles = []
-                             , writeSuppressionFiles = False
-                             , logger = CliCommunication.dummy
-                             , suppress = False
-                             , watch = False
-                             , supportsColor = True
-                             , debug = False
-                             }
+                            (Flags.default
                              -- , abort <| "Problem decoding the flags when running the elm-review runner:\n  " ++ Decode.errorToString error
                             )
             in
             initWithFlags env fs flags
 
 
-initWithFlags : Env -> FileSystem -> DecodedFlags -> ( ModelWrapper, Cmd Msg )
+initWithFlags : Env -> FileSystem -> Flags -> ( ModelWrapper, Cmd Msg )
 initWithFlags env fs flags =
     case computeRulesToRun env flags of
         Err cmd ->
@@ -315,7 +284,7 @@ initWithFlags env fs flags =
             initValid env fs flags rules
 
 
-initValid : Env -> FileSystem -> DecodedFlags -> List Rule -> ( ModelWrapper, Cmd Msg )
+initValid : Env -> FileSystem -> Flags -> List Rule -> ( ModelWrapper, Cmd Msg )
 initValid env fs flags rulesFromConfig =
     let
         rules : List Rule
@@ -385,7 +354,7 @@ initValid env fs flags rulesFromConfig =
     )
 
 
-computeRulesToRun : Env -> DecodedFlags -> Result (Cmd msg) (List Rule)
+computeRulesToRun : Env -> Flags -> Result (Cmd msg) (List Rule)
 computeRulesToRun env flags =
     let
         rulesWithIds : List Rule
@@ -515,176 +484,6 @@ closestNames names name =
     names
         |> List.sortBy (Levenshtein.distance name)
         |> List.take 3
-
-
-type alias DecodedFlags =
-    { fixMode : FixMode
-    , fixLimit : Maybe Int
-    , fixExplanation : FixExplanation
-    , enableExtract : Bool
-    , unsuppressMode : UnsuppressMode
-    , detailsMode : Reporter.DetailsMode
-    , reportMode : ReportMode
-    , ignoreProblematicDependencies : Bool
-    , rulesFilter : Maybe (Set String)
-    , ignoredDirs : List String
-    , ignoredFiles : List String
-    , writeSuppressionFiles : Bool
-    , logger : CliCommunication.Key
-    , suppress : Bool
-    , watch : Bool
-    , supportsColor : Bool
-    , debug : Bool
-    }
-
-
-decodeFlags : Decode.Decoder DecodedFlags
-decodeFlags =
-    Decode.succeed toDecodedFlags
-        |> field "fixMode" decodeFix
-        |> field "fixLimit" decodeFixLimit
-        |> field "fileRemovalFixesEnabled" Decode.bool
-        |> field "explainFixFailure" Decode.bool
-        |> field "enableExtract" Decode.bool
-        |> field "unsuppress" UnsuppressMode.decoder
-        |> field "detailsMode" decodeDetailsMode
-        |> field "report" decodeReportMode
-        |> field "ignoreProblematicDependencies" Decode.bool
-        |> field "rulesFilter" decodeRulesFilter
-        |> field "ignoredDirs" (Decode.list Decode.string)
-        |> field "ignoredFiles" (Decode.list Decode.string)
-        |> field "writeSuppressionFiles" Decode.bool
-        |> field "logger" CliCommunication.decoder
-        |> field "suppress" Decode.bool
-        |> field "watch" Decode.bool
-        |> field "color" Decode.bool
-        |> field "debug" Decode.bool
-
-
-toDecodedFlags :
-    (Bool -> FixMode)
-    -> Maybe Int
-    -> Bool
-    -> Bool
-    -> Bool
-    -> UnsuppressMode
-    -> Reporter.DetailsMode
-    -> ReportMode
-    -> Bool
-    -> Maybe (Set String)
-    -> List String
-    -> List String
-    -> Bool
-    -> CliCommunication.Key
-    -> Bool
-    -> Bool
-    -> Bool
-    -> Bool
-    -> DecodedFlags
-toDecodedFlags fixMode fixLimit fileRemovalFixesEnabled explainFixFailure enableExtract unsuppressMode detailsMode reportMode ignoreProblematicDependencies rulesFilter ignoredDirs ignoredFiles writeSuppressionFiles logger suppress watch supportsColor debug =
-    { fixMode = fixMode fileRemovalFixesEnabled
-    , fixLimit = fixLimit
-    , fixExplanation =
-        if explainFixFailure then
-            FixExplanation.Detailed
-
-        else
-            FixExplanation.Succinct
-    , enableExtract = enableExtract
-    , unsuppressMode = unsuppressMode
-    , detailsMode = detailsMode
-    , reportMode = reportMode
-    , ignoreProblematicDependencies = ignoreProblematicDependencies
-    , rulesFilter = rulesFilter
-    , ignoredDirs = ignoredDirs
-    , ignoredFiles = ignoredFiles
-    , writeSuppressionFiles = writeSuppressionFiles
-    , logger = logger
-    , suppress = suppress
-    , watch = watch
-    , supportsColor = supportsColor
-    , debug = debug
-    }
-
-
-field : String -> Decode.Decoder a -> Decode.Decoder (a -> b) -> Decode.Decoder b
-field key valDecoder decoder =
-    Decode.map2
-        (|>)
-        (Decode.field key valDecoder)
-        decoder
-
-
-decodeFix : Decode.Decoder (Bool -> FixMode)
-decodeFix =
-    Decode.string
-        |> Decode.andThen
-            (\fixMode ->
-                case fixMode of
-                    "dontfix" ->
-                        Decode.succeed (always Mode_DontFix)
-
-                    "fix" ->
-                        Decode.succeed Mode_Fix
-
-                    "fixAll" ->
-                        Decode.succeed Mode_FixAll
-
-                    _ ->
-                        Decode.fail <| "I could not understand the following fix mode: " ++ fixMode
-            )
-
-
-decodeFixLimit : Decode.Decoder (Maybe Int)
-decodeFixLimit =
-    Decode.oneOf
-        [ Decode.int
-            |> Decode.map (\n -> Just (max 1 n))
-        , Decode.null Nothing
-        ]
-
-
-decodeDetailsMode : Decode.Decoder Reporter.DetailsMode
-decodeDetailsMode =
-    Decode.string
-        |> Decode.andThen
-            (\detailsMode ->
-                case detailsMode of
-                    "with-details" ->
-                        Decode.succeed Reporter.WithDetails
-
-                    "without-details" ->
-                        Decode.succeed Reporter.WithoutDetails
-
-                    _ ->
-                        Decode.fail <| "I could not understand the following details mode: " ++ detailsMode
-            )
-
-
-decodeReportMode : Decode.Decoder ReportMode
-decodeReportMode =
-    Decode.string
-        |> Decode.andThen
-            (\reportMode ->
-                case reportMode of
-                    "human" ->
-                        Decode.succeed HumanReadable
-
-                    "json" ->
-                        Decode.succeed Json
-
-                    _ ->
-                        Decode.fail <| "I could not understand the following report mode: " ++ reportMode
-            )
-
-
-decodeRulesFilter : Decode.Decoder (Maybe (Set String))
-decodeRulesFilter =
-    Decode.oneOf
-        [ Decode.list Decode.string
-            |> Decode.map (Set.fromList >> Just)
-        , Decode.null Nothing
-        ]
 
 
 
