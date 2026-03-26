ac7b191c1441df9efbb916fb3f97fa3ce49eb288
Jeroen Engels <jfm.engels@gmail.com>
2026-03-26 16:56:14 +0100

    Start a new Wrapper application that spawns the binary given as flag

---
+++ wrapper/src/WrapperMain.elm
@@ -0,0 +1,122 @@
+module WrapperMain exposing (main)
+
+import Cli exposing (Env)
+import Fs exposing (FileSystem)
+import Os exposing (ProcessCapability)
+import Os.Process as Process exposing (ProcessError, defaultSpawnOptions)
+import Task
+import Wrapper.Flags as Flags
+
+
+main : Cli.Program ModelWrapper Msg
+main =
+    Cli.program
+        { init = init
+        , update = updateWrapper
+        , subscriptions = \_ -> Sub.none
+        }
+
+
+type ModelWrapper
+    = Done
+    | Running Model
+
+
+type alias Model =
+    { env : Env
+    , fs : FileSystem
+    }
+
+
+type Msg
+    = ReviewProcessEnded (Result ProcessError Process.Completed)
+
+
+init : Env -> ( ModelWrapper, Cmd Msg )
+init env =
+    case requireCapabilities env of
+        Err msg ->
+            ( Done
+            , Cmd.batch
+                [ Cli.println env.stderr (env.programName ++ ": " ++ msg)
+                , Cli.exit 1
+                ]
+            )
+
+        Ok { fs, os } ->
+            case Flags.parse env of
+                Err error ->
+                    ( Done
+                    , Cmd.batch
+                        [ Cli.println env.stderr error
+                        , Cli.exit 1
+                        ]
+                    )
+
+                Ok flags ->
+                    ( Running { env = env, fs = fs }
+                    , Process.run os
+                        flags.appBinary
+                        { defaultSpawnOptions
+                            | args = []
+                            , stdout = Process.InheritStdout
+                            , stderr = Process.InheritStderr
+                        }
+                        |> Task.attempt ReviewProcessEnded
+                    )
+
+
+requireCapabilities : Env -> Result String { fs : FileSystem, os : ProcessCapability }
+requireCapabilities env =
+    case Fs.require env of
+        Err msg ->
+            Err (env.programName ++ ": " ++ msg)
+
+        Ok fs ->
+            case Os.requireProcess env of
+                Err msg ->
+                    Err (env.programName ++ ": " ++ msg)
+
+                Ok os ->
+                    Ok { fs = fs, os = os }
+
+
+updateWrapper : Msg -> ModelWrapper -> ( ModelWrapper, Cmd Msg )
+updateWrapper msg wrapper =
+    case wrapper of
+        Done ->
+            ( wrapper, Cmd.none )
+
+        Running model ->
+            update msg model
+                |> Tuple.mapFirst Running
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update (ReviewProcessEnded result) model =
+    case result of
+        Ok completed ->
+            ( model
+            , Cli.exit completed.exitCode
+            )
+
+        Err err ->
+            ( model
+            , Cmd.batch
+                [ Cli.println model.env.stdout ("error: " ++ processErrorToString err)
+                , Cli.exit 1
+                ]
+            )
+
+
+processErrorToString : ProcessError -> String
+processErrorToString err =
+    case err of
+        Process.PermissionDenied ->
+            "PermissionDenied"
+
+        Process.CaptureLimitExceeded stream ->
+            "CaptureLimitExceeded(" ++ stream ++ ")"
+
+        Process.ProcessError message ->
+            message
