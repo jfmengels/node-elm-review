bec3f1e7177d1d2c2cd6d9f3fa0e275becd4bae3
Jeroen Engels <jfm.engels@gmail.com>
2026-03-26 10:20:11 +0100

    Clean up unused code

--- template/src/Elm/Review/UnsuppressMode.elm
+++ template/src/Elm/Review/UnsuppressMode.elm
@@ -1,6 +1,5 @@
-module Elm.Review.UnsuppressMode exposing (UnsuppressMode(..), decoder)
+module Elm.Review.UnsuppressMode exposing (UnsuppressMode(..))
 
-import Json.Decode as Decode
 import Set exposing (Set)
 
 
@@ -8,20 +7,3 @@ type UnsuppressMode
     = UnsuppressAll
     | UnsuppressRules (Set String)
     | UnsuppressNone
-
-
-decoder : Decode.Decoder UnsuppressMode
-decoder =
-    Decode.oneOf
-        [ Decode.list Decode.string
-            |> Decode.map (Set.fromList >> UnsuppressRules)
-        , Decode.bool
-            |> Decode.map
-                (\bool ->
-                    if bool then
-                        UnsuppressAll
-
-                    else
-                        UnsuppressNone
-                )
-        ]
