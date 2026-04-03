2adda182595997a7ca0c04640085e0a345f9f016
Jeroen Engels <jfm.engels@gmail.com>
2026-04-03 11:40:11 +0200

    Copy template files

--- elm-run/src/ElmRun/OsExtra.elm
+++ elm-run/src/ElmRun/OsExtra.elm
@@ -1,6 +1,8 @@
-module ElmRun.OsExtra exposing (errorToString)
+module ElmRun.OsExtra exposing (errorToString, which)
 
+import Os exposing (ProcessCapability)
 import Os.Process as Process exposing (ProcessError)
+import Task exposing (Task)
 
 
 errorToString : ProcessError -> String
@@ -14,3 +16,19 @@ errorToString err =
 
         Process.ProcessError message ->
             message
+
+
+{-| Find the path to a command.
+-}
+which : ProcessCapability -> String -> Task ProcessError (Maybe String)
+which os command =
+    Process.run os
+        "which"
+        { cwd = Nothing
+        , env = Nothing
+        , args = [ command ]
+        , stdin = Process.NullStdin
+        , stdout = Process.CaptureStdout { maxBytes = 4096, onOverflow = Process.TruncateOutput }
+        , stderr = Process.NullStderr
+        }
+        |> Task.map .stdout
