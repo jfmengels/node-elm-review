5bb9a989d52e14d936668c52f2dcbbb9c66c51eb
Jeroen Engels <jfm.engels@gmail.com>
2026-04-03 11:36:00 +0200

    Move CopyDirectory to FsExtra

--- elm-run/src/ElmRun/FsExtra.elm
+++ elm-run/src/ElmRun/FsExtra.elm
@@ -1,6 +1,9 @@
-module ElmRun.FsExtra exposing (errorToString)
+module ElmRun.FsExtra exposing (copyDirectory, errorToString)
 
 import Fs exposing (FsError)
+import Os exposing (ProcessCapability)
+import Os.Process as Process exposing (ProcessError)
+import Task exposing (Task)
 
 
 errorToString : FsError -> String
@@ -14,3 +17,22 @@ errorToString fsError =
 
         Fs.IoError msg ->
             "Unknown error: " ++ msg
+
+
+{-| Remove this when elm-run provides this functionality.
+
+TODO Also remove the dependency to elm-run/os in Build.addReviewAppDependencies
+
+-}
+copyDirectory : ProcessCapability -> { from : String, to : String } -> Task ProcessError ()
+copyDirectory os { from, to } =
+    Process.run os
+        "cp"
+        { cwd = Nothing
+        , env = Nothing
+        , args = [ "-R", from, to ]
+        , stdin = Process.NullStdin
+        , stdout = Process.NullStdout
+        , stderr = Process.NullStderr
+        }
+        |> Task.map (\_ -> ())
