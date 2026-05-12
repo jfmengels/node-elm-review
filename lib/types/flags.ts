c703cca714b32f03273ed57e6ecc71cf39f8a088
Jeroen Engels <jfm.engels@gmail.com>
2026-05-12 13:54:48 +0200

    Spawn a Node.js script that loads the compiled Elm module

--- lib/types/flags.ts
+++ lib/types/flags.ts
@@ -1,13 +1,4 @@
-import type {FixMode} from "./fix.ts";
-import type {OptionsBase} from "./options.ts";
-
-export type Flags = OptionsBase & {
-  resultCacheFolder: string;
-  fixMode: FixMode;
-  fixLimit: number | null;
-  directoriesToAnalyze: string[];
-  ignoredDirs: string[];
-  ignoredFiles: string[];
-  writeSuppressionFiles: boolean;
+export type Flags = {
+  reviewAppFlags: string[];
   logger?: unknown;
 };
