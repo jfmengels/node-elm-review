a1ce11451e26f62547b92fcfda0e5a629a6e5dd5
Jeroen Engels <jfm.engels@gmail.com>
2026-05-13 23:47:13 +0200

    Pass default output target

--- node/src/Node/WrapperMain.elm
+++ node/src/Node/WrapperMain.elm
@@ -1,13 +1,14 @@
 module Node.WrapperMain exposing (main)
 
 import Node.Program
+import Wrapper.OutputTarget as OutputTarget
 import WrapperMain
 
 
 main : Node.Program.Program WrapperMain.Model WrapperMain.Msg
 main =
     Node.Program.program
-        { init = \{ env, args } -> WrapperMain.init env args True
+        { init = \{ env, args } -> WrapperMain.init env args True OutputTarget.JavaScriptTarget
         , update = WrapperMain.update
         , subscriptions = WrapperMain.subscriptions
         }
